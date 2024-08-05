ea903df624fb9f8742b72e597c001bc1b391aff8
Charity-spoko <liwewecharity@gmail.com>
2024-08-05 16:33:05 +0200

    initial add

---
+++ pra.js
@@ -0,0 +1,22 @@
+/*x=5;
+y=10;
+z=15;
+result=(x+y)*z
+console.log(result);*/
+/*fullname="CharitySpoko";
+hie="Hello"
+result=hie+fullname;
+console.log(result);*/
+/*r=5;
+p=3.14;
+Area=p*r^2;
+console.log(Area);*/
+let num1=5;
+let num2=10;
+
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2;
+
+console.log("num1 =",num1);
+console.log("num2 =",num2);
